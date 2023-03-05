8ff0416e4bf307d2f4f5c67d8075ebb73f6b1691
msiwak <marcin.siwak@iteo.com>
2023-03-05 15:10:26 +0100

    changed dialog color

--- androidApp/src/main/java/pl/msiwak/multiplatform/android/ui/category/AddExerciseDialog.kt
+++ androidApp/src/main/java/pl/msiwak/multiplatform/android/ui/category/AddExerciseDialog.kt
@@ -1,9 +1,15 @@
 package pl.msiwak.multiplatform.android.ui.category
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import pl.msiwak.multiplatform.android.ui.components.InputView
 
 @Composable
@@ -14,6 +20,8 @@ fun AddExerciseDialog(
     onDialogClosed: () -> Unit = {}
 ) {
     AlertDialog(
+        shape = RoundedCornerShape(16.dp),
+        backgroundColor = Color.DarkGray,
         onDismissRequest = {
             onDialogClosed()
         },
@@ -28,6 +36,9 @@ fun AddExerciseDialog(
         },
         confirmButton = {
             Button(
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
                 onClick = {
                     onAddExerciseClicked()
                 }) {
