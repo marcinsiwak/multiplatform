4b72a1fcd47df136c1a846ec3c65481ed2e931e0
msiwak <marcin.siwak@iteo.com>
2023-04-20 10:23:11 +0200

    beta version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 val versionMajor = 0
 val versionMinor = 0
-val versionPatch = 0
+val versionPatch = 1
 val versionBuild = 0
 val versionCode = 1_000_000 * versionMajor + 10_000 * versionMinor + 100 * versionPatch + versionBuild
 
