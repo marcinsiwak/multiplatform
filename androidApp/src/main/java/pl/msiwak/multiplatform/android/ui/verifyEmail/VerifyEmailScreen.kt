2556dc6d336d8ed5424aa9d1f8bbb3e6f5671033
msiwak <marcin.siwak@iteo.com>
2023-07-19 14:57:41 +0200

    pretty view

--- androidApp/src/main/java/pl/msiwak/multiplatform/android/ui/verifyEmail/VerifyEmailScreen.kt
+++ androidApp/src/main/java/pl/msiwak/multiplatform/android/ui/verifyEmail/VerifyEmailScreen.kt
@@ -1,12 +1,14 @@
 package pl.msiwak.multiplatform.android.ui.verifyEmail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.flow.collectLatest
@@ -27,14 +29,19 @@ fun VerifyEmailScreen() {
 
     LaunchedEffect(key1 = Unit) {
         viewModel.viewEvent.collectLatest {
-            when(it) {
+            when (it) {
                 VerifyEmailEvent.OpenMail -> context.openMailApp()
             }
         }
     }
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                vertical = MaterialTheme.dimens.space_32,
+                horizontal = MaterialTheme.dimens.space_16
+            ),
     ) {
         Text(
             modifier = Modifier.padding(MaterialTheme.dimens.space_8),
@@ -43,24 +50,26 @@ fun VerifyEmailScreen() {
         )
         Text(
             modifier = Modifier.padding(MaterialTheme.dimens.space_8),
-            text = "Check your email",
+            text = "We have sent you activation link. Please check your email and verify your account.",
             fontSize = MaterialTheme.font.font_16
         )
 
+        Spacer(modifier = Modifier.weight(1f))
+
         MainButton(
-            modifier = Modifier,
+            modifier = Modifier.fillMaxWidth().padding(vertical = MaterialTheme.dimens.space_8),
             onClick = { viewModel.onOpenMailClicked() },
             text = "Open email"
         )
 
         SecondaryButton(
-            modifier = Modifier,
+            modifier = Modifier.fillMaxWidth(),
             onClick = { viewModel.onResendMailClicked() },
             text = "Resend email"
         )
 
         MainButton(
-            modifier = Modifier,
+            modifier = Modifier.fillMaxWidth().padding(vertical = MaterialTheme.dimens.space_8),
             onClick = { viewModel.onLoginClicked() },
             text = "Login"
         )
