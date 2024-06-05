9bc7ce8f2e05b317716e55ff003a773262aae7e5
msiwak <marcinsiwak15@gmail.com>
2024-06-05 16:37:09 +0200

    google icon

--- shared/ui-common-component/src/commonMain/kotlin/pl/msiwak/multiplatform/ui/commonComponent/SecondaryButton.kt
+++ shared/ui-common-component/src/commonMain/kotlin/pl/msiwak/multiplatform/ui/commonComponent/SecondaryButton.kt
@@ -1,16 +1,18 @@
 package pl.msiwak.multiplatform.ui.commonComponent
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import pl.msiwak.multiplatform.commonResources.theme.dimens
 
 @Composable
 fun SecondaryButton(modifier: Modifier = Modifier, onClick: () -> Unit, text: String) {
     Button(
-        modifier = modifier,
+        modifier = modifier.height(MaterialTheme.dimens.default_button_height),
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.tertiary,
