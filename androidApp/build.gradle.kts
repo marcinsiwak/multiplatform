62a0bdfb7d02c895d5743a4247e2005e2a9a081c
msiwak <marcinsiwak15@gmail.com>
2024-07-18 12:06:41 +0200

    Prepare release 1.1.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 }
 
 val versionMajor = 1
-val versionMinor = 0
-val versionPatch = 1
+val versionMinor = 1
+val versionPatch = 0
 val versionBuild = 0
 val versionCode =
     1_000_000 * versionMajor + 10_000 * versionMinor + 100 * versionPatch + versionBuild
