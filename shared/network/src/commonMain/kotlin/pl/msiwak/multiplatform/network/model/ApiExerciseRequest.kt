a97bfedcc195b4ecd09e259df80b1ae34dfb8034
msiwak <marcinsiwak15@gmail.com>
2024-09-16 00:25:50 +0200

    removed unused

--- shared/network/src/commonMain/kotlin/pl/msiwak/multiplatform/network/model/ApiExerciseRequest.kt
+++
@@ -1,9 +0,0 @@
-package pl.msiwak.multiplatform.network.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class ApiExerciseRequest(
-    val categoryId: String,
-    val name: String
-)
