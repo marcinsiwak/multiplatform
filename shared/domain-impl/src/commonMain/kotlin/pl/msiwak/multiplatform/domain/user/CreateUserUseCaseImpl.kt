7f4f67f29c87133df6cdd9cf91c70ea4aeaf1979
marcinsiwak <52464613+marcinsiwak@users.noreply.github.com>
2024-11-12 21:54:43 +0100

    fixes

--- shared/domain-impl/src/commonMain/kotlin/pl/msiwak/multiplatform/domain/user/CreateUserUseCaseImpl.kt
+++
@@ -1,9 +0,0 @@
-package pl.msiwak.multiplatform.domain.user
-
-import pl.msiwak.multiplatform.data.remote.repository.UserRepository
-
-class CreateUserUseCaseImpl(private val userRepository: UserRepository) : CreateUserUseCase {
-    override suspend fun invoke(uuid: String) {
-        userRepository.createUser(uuid)
-    }
-}
