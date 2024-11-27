fb0cc9fd79716e7dc8852ebf1647266e0e588cdc
msiwak <marcinsiwak15@gmail.com>
2024-11-27 11:16:47 +0100

    prepare composeApp vol2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "athletetrack"
 include(":composeApp")
-include(":androidApp")
+//include(":androidApp")
 include(":shared")
 include(":shared-mobile")
 include(":shared-mobile:commonResources")
