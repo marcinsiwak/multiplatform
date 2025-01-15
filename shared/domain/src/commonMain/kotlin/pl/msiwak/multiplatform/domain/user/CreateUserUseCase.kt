a9c7272d4be004b3e322ea00d2d382f3ec866f92
marcinsiwak <52464613+marcinsiwak@users.noreply.github.com>
2025-01-15 10:11:38 +0100

    fixes (#79)

--- shared/domain/src/commonMain/kotlin/pl/msiwak/multiplatform/domain/user/CreateUserUseCase.kt
+++
@@ -1,5 +0,0 @@
-package pl.msiwak.multiplatform.domain.user
-
-interface CreateUserUseCase {
-    suspend operator fun invoke(uuid: String)
-}
--- shared/ui-welcome/src/commonMain/kotlin/pl/msiwak/multiplatform/ui/welcome/terms/TermsConfirmationUiAction.kt
+++
@@ -1,8 +0,0 @@
-package pl.msiwak.multiplatform.ui.welcome.terms
-
-sealed class TermsConfirmationUiAction {
-    data object OnConfirmSynchronizationClicked : TermsConfirmationUiAction()
-    data object OnDismissSynchronizationClicked : TermsConfirmationUiAction()
-    data class OnButtonClick(val uuid: String) : TermsConfirmationUiAction()
-    data object OnTermsClick : TermsConfirmationUiAction()
-}
